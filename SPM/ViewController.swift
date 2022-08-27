eb50b70a6fa02375b8ac57c08c2c1752532d7e51
MinoriHaruta <hrtmnr.3072@gmail.com>
2022-08-27 20:59:01 +0900

    😞

--- SPM/ViewController.swift
+++ SPM/ViewController.swift
@@ -6,12 +6,18 @@
 //
 
 import UIKit
+import PKHUD
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        HUD.flash(.success, delay: 2.0)
+        
     }
 
 
